b309e2e841bfc6a04238c6edce6940b1a7c9d343
Sumit <sumityadav0376@gmail.com>
2021-05-26 14:24:30 +0530

    Changing the state

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
   }
 
   render() {
-    const movies = this.props.store.getState();
+    const { list } = this.props.store.getState();   // {list: [], favourites: []}
     console.log("RENDER");
 
     return (
@@ -35,7 +35,7 @@ class App extends React.Component {
           </div>
   
           <div className="list">
-            {movies.map((movie, index) => (
+            {list.map((movie, index) => (
               <MovieCard movie={movie} key={`movies-${index}`} />
             ))}
           </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,19 @@
 import { ADD_MOVIES } from '../actions';
 
-export default function movies(state = [], action){
+const initialMoviesState = {
+    list: [],
+    favourites: []
+};
+
+export default function movies(state = initialMoviesState, action){
     if(action.type === ADD_MOVIES){
-        return action.movies;
+        // We cant modify the state (state object)
+        // Therefore, new object need to be crearted having same properties.....  Using spread operator
+
+        return {
+            ...state,
+            list: action.movies
+        }
     }
     
     return state;
