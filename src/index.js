7bb85b6b8bb84305a1b7a591af2765497a26a1e0
Sumit <sumityadav0376@gmail.com>
2021-05-30 23:02:13 +0530

    Middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 
 import './index.css';
@@ -8,7 +8,19 @@ import App from './components/App';
 import rootReducer from './reducers';
 
 
-const store = createStore(rootReducer);
+// function logger(obj, next, action)
+// logger(obj)(next)(action)  -->> Internally redux will call our middleware like this
+const logger = function({dispatch, getStore}) {
+  return function(next) {
+    return function(action ){
+      // middleware code
+      console.log('ACTION_TYPE = ', action.type);
+      next(action);   // this will call the next middleware. And if it is the last middleware, then dispatch is called
+    }
+  }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('store', store);
 
 ReactDOM.render(
